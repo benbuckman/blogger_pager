d34ae7fdb5c56b7eb0eead7177aabb30b6bf0c19
Ben Buckman, New Leaf Digital <ben@newleafdigital.com>
2011-11-09 21:39:08 -0300

    app parses then runs server

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
 
-/**
- * Module dependencies.
- */
+var runServerWithPosts = function(posts) {
+  var express = require('express');
 
-var express = require('express');
+  var app = module.exports = express.createServer();
 
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// Routes
-
-app.get('/', function(req, res){
-  res.render('index', {
-    title: 'Express'
+  app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    // app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
   });
-});
 
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  app.configure('development', function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  });
+
+  app.configure('production', function(){
+    app.use(express.errorHandler()); 
+  });
+
+  app.get('/', function(req, res){
+    res.render('index', {
+      title: 'Express'
+    });
+  });
+
+  app.listen(3000);
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+};
+
+
+// parse the xml, load the paths into memory, then run the server
+var parser = require('./parse-urls');
+var xmlFilePath = __dirname + '/blog-export.xml';
+console.log(xmlFilePath);
+
+parser.parseBloggerXml(xmlFilePath, function parserCallback(err, posts) {
+  if (err) throw err;
+  else runServerWithPosts(posts);
+});
--- parse-urls.js
+++ parse-urls.js
@@ -2,27 +2,66 @@
     runs as script, not a server!
 **/
 
-var fs = require('fs')
+exports.parseBloggerXml = function(xmlFilePath, callback) {
 
-// tried node-o3-xml and node-o3-fastxml, didn't work;
-// tried node-xml, but that's too complicated.
-  , xml = require('xml2js')
-  , parser = new xml.Parser()
-  , xmlFilePath = './blog-export.xml'
-  ;
+  var fs = require('fs')
+  
+    // tried node-o3-xml and node-o3-fastxml, didn't work;
+    // tried node-xml, but that's too complicated.
+    , xml = require('xml2js')
+    , parser = new xml.Parser()
+    , _ = require('underscore')._
+    ;
+  
+  console.log("Reading file...");
+  fs.readFile(xmlFilePath, function readFileCallback(err, xmlText) {
 
-console.log("Reading file...");
-fs.readFile(xmlFilePath, function readFileCallback(err, xmlText) {
-  
-  if (err) {
-    throw err;
-    return;
-  }
-  
-  console.log("Got file, parsing...");
-  
-  parser.parseString(xmlText, function parseXmlCallback (err, result) {
-    console.dir(result);
-  });
-    
-});
+    if (err) return callback(err);
+
+    console.log("Got file, parsing...");
+
+    parser.parseString(xmlText, function parseXmlCallback (err, result) {
+      if (err) return callback(err);
+
+      var posts = [];
+
+      if (_.isUndefined(result.entry)) {
+        callback(new Error("XML is missing 'entry' array."));
+      }
+
+      _(result.entry).each(function eachEntryCallback(entry) {
+
+        // only want the posts
+        if (_.isUndefined(entry.category['@'])) return;
+        if (_.isUndefined(entry.category['@'].term)) return;
+
+        if (entry.category['@'].term.indexOf("#post") != -1 && !_.isUndefined(entry.link)) {
+
+          _(entry.link).each(function linkCallback(link) {
+            if (_.isUndefined(link['@'])) return;
+            link = link['@'];
+
+            if (link.type == 'text/html' && link.href.indexOf(".html") != -1) {
+
+              // it's a post!
+              posts.push({
+                url: link.href,
+                date: entry.published
+              });
+
+            }
+          });
+
+          // console.dir(entry.link['@'].href);
+        }
+
+      });
+
+      console.log("Got %d post URLs", posts.length);
+      callback(null, posts);
+    });
+
+  }); //readFile
+
+  return;
+};  //parseBloggerXml
